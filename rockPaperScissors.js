bdf36a62f8cb26781810f9a5c2dcdddf1f88aea3
LeSparkly <harley.ray2000@gmail.com>
2024-05-18 01:28:31 -0400

    Update references to HTML classes to reflect name changes

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,14 +5,13 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-const humanScoreDisplay = document.querySelector("#player-score");
-const computerScoreDisplay = document.querySelector(".computer-score");
-const humanMoveDisplay = document.querySelector(".human-move");
+const humanScoreDisplay = document.querySelector("#player.score");
+const computerScoreDisplay = document.querySelector(".computer.score");
+const humanMoveDisplay = document.querySelector(".human.move");
 const computerMoveDisplay = document.querySelector(".computer.move");
 let roundWinnerDisplay = document.querySelector(".round.winner");
 
-const playButton = document.querySelector(".play-button");
-const buttonSpan = document.querySelector(".buttons");
+const playButton = document.querySelector(".play.button");
 
 playButton.addEventListener("click", () => {
   playGame();
