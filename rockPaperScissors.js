e259cd86d14c6bd91a44d360ec9a52a6d8da6139
LeSparkly <harley.ray2000@gmail.com>
2024-05-18 02:37:13 -0400

    Update odds to attempt to make it more random

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -37,7 +37,7 @@ function getComputerChoice() {
   let randomNumber = Math.random();
   if (randomNumber <= 0.33) {
     return "Rock";
-  } else if (randomNumber >= 0.67) {
+  } else if (randomNumber > 0.33 && randomNumber < 0.66) {
     return "Paper";
   } else {
     return "Scissors";
